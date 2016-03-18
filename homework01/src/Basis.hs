c10ea462b95400cc58fbbb543a919b31986383a1
CristhianMotoche <cmotoche@stackbuilders.com>
2016-03-18 14:24:52 -0500

    Solve issue related with spaces in Homework 01

--- homework01/src/Basis.hs
+++ homework01/src/Basis.hs
@@ -73,7 +73,7 @@ type Move = (Peg, Peg)
 -- |
 --
 -- >>> hanoi 2 "a" "b" "c"
--- [("a","c"), ("a","b"), ("c","b")]
+-- [("a","c"),("a","b"),("c","b")]
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi = undefined
