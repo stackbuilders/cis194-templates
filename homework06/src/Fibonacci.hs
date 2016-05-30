0b71ad6460f824eb14be580864013b5b31eaf42f
flandrade <flandradeg@gmail.com>
2016-05-30 16:27:14 -0500

    Add empty declaration of Stream

--- homework06/src/Fibonacci.hs
+++ homework06/src/Fibonacci.hs
@@ -30,46 +30,48 @@ fibs2 = undefined
 -- Exercise 3
 ----------------------------------------------------------------------
 
---streamToList :: Stream a -> [a]
---streamToList = undefined
+data Stream a
+
+streamToList :: Stream a -> [a]
+streamToList = undefined
 
 
 ----------------------------------------------------------------------
 -- Exercise 4
 ----------------------------------------------------------------------
 
---streamRepeat :: a -> Stream a
---streamRepeat = undefined
+streamRepeat :: a -> Stream a
+streamRepeat = undefined
 
---streamMap :: (a -> b) -> Stream a -> Stream b
---streamMap = undefined
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap = undefined
 
---streamFromSeed :: (a -> a) -> a -> Stream a
---streamFromSeed = undefined
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed = undefined
 
 
 ----------------------------------------------------------------------
 -- Exercise 5
 ----------------------------------------------------------------------
 
---nats :: Stream Integer
---nats = undefined
+nats :: Stream Integer
+nats = undefined
 
---ruler :: Stream Integer
---ruler = undefined
+ruler :: Stream Integer
+ruler = undefined
 
 
 ----------------------------------------------------------------------
 -- Exercise 6 (Optional)
 ----------------------------------------------------------------------
 
---x :: Stream Integer
---x = undefined
+x :: Stream Integer
+x = undefined
 
 
 ----------------------------------------------------------------------
 -- Exercise 7 (Optional)
 ----------------------------------------------------------------------
 
---fib4 :: Integer -> Integer
---fib4 = undefined
+fib4 :: Integer -> Integer
+fib4 = undefined
