78c0db3699cc5d72dfea0265967fc56957c2b8f2
Juan Pedro Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2019-05-16 14:28:48 -0500

    Add args to examples test suite

---
+++ Makefile
@@ -0,0 +1,2 @@
+homework-03-examples:
+	stack build --file-watch --test :doctest --test-arguments src/Homework03/Golf.hs
--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
+-- base
+import System.Environment (getArgs)
+
+-- doctest
 import Test.DocTest
 
 main :: IO ()
-main = doctest ["src"]
+main = do
+  args <- getArgs
+  doctest (if null args then ["src"] else args)
